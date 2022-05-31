4fad3c8c018eb14d780bb70f2fbb6524d9f767ef
Michael Feurstein <michael.feurstein@wu.ac.at>
2022-05-31 18:05:32 +0200

    apply tests for sus score ranking

--- pretest-analysis.R
+++ pretest-analysis.R
@@ -233,3 +233,79 @@ ggplot(Sum,
                          "of the median."),
        hjust=0.5)
 
+##
+## Statistical tests for SUS score question 
+##
+
+# Friedman's test based on: https://stats.stackexchange.com/questions/142903/develop-a-statistical-test-to-distinguish-two-products
+friedman.test(sus ~ notation.r|id, data = data)
+
+# Wilcoxon rank sum test
+#data$sus <- as.numeric(data$sus)
+
+data %>% sample_n_by(notation.r, size = 2)
+
+data %>%
+  group_by(notation.r) %>%
+  get_summary_stats(sus, type = "median_iqr")
+
+bxp <- ggboxplot(
+  data, x = "notation.r", y ="sus",
+  ylab = "SUS score", xlab = "Notation", add = "jitter"
+)
+bxp
+
+stat.test <- data %>%
+  wilcox_test(sus ~ notation.r) %>%
+  add_significance()
+stat.test
+
+data %>% wilcox_effsize(sus ~ notation.r)
+
+stat.test <- stat.test %>% add_xy_position(x = "notation.r")
+bxp + 
+  stat_pvalue_manual(stat.test, tip.length = 0) +
+  labs(subtitle = get_test_label(stat.test, detailed = TRUE))
+
+# Aligned ranks transformation ANOVA (ART anova)
+# Based on: https://rcompanion.org/handbook/F_16.html
+
+ar.model <- art(sus ~ experience.r + notation.r + experience.r:notation.r, data = data)
+ar.model
+anova(ar.model)
+
+library(rcompanion)
+Sum = groupwiseMedian(sus ~ experience.r + notation.r,
+                      data=data,
+                      bca=FALSE, percentile=TRUE)
+Sum
+
+pd = position_dodge(0.3)
+ggplot(Sum,
+       aes(x = notation.r,
+           y = Median,
+           color = experience.r)) + 
+  geom_point(shape  = 15,
+             size   = 4,
+             position = pd) + 
+  geom_errorbar(aes(ymin  =  Percentile.lower,
+                    ymax  =  Percentile.upper),
+                width =  0.2,
+                size  =  0.7,
+                position = pd) + 
+  theme_bw() + 
+  theme(axis.title   = element_text(face = "bold"),
+        axis.text    = element_text(face = "bold"),
+        plot.caption = element_text(hjust = 0)) + 
+  ylab("Median SUS score count") + 
+  ggtitle ("SUS score counts for advanced and novice users",
+           subtitle = "for two types of notations") + 
+  
+  labs(caption  = paste0("\nSUS score counts for two experience levels ",
+                         "and two types of notation. Boxes indicate \n",
+                         "the median. ",
+                         "Error bars indicate the 95% confidence ",
+                         "interval ",
+                         "of the median."),
+       hjust=0.5)
+
