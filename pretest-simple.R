d1b29e80601f30a04cdc054ff07563e7acc7c0ae
Michael Feurstein <michael.feurstein@wu.ac.at>
2022-06-09 13:54:09 +0200

    update steps for preparing data

--- pretest-simple.R
+++ pretest-simple.R
@@ -7,22 +7,20 @@ datasimple$duration <- as.numeric(format(datasimple$time, "%M")) + as.numeric(fo
 # preparations for testing
 # setup mapping and factors
 syntaxMapping <- c("nl" = 1, "kv" = 2)
-experienceMapping <- c("advanced" = 3)
 rankingMapping <- c("don't know" = 0, "2nd place" = 1, "1st place" = 2)
 
 datasimple$notation.r <- syntaxMapping[datasimple$notation]
-datasimple$experience.r <- experienceMapping[datasimple$experience]
 datasimple$rank.r <- rankingMapping[datasimple$rank]
 
 datasimple$notation.r <- as.factor(datasimple$notation.r)
 datasimple$notation.r <- factor(datasimple$notation.r, levels = c("1", "2"), labels = c("natural language", "key-value"))
-datasimple$experience.r <- as.factor(datasimple$experience.r)
-datasimple$experience.r <- factor(datasimple$experience.r, levels = c("3"), labels = c("advanced"))
 datasimple$sequence <- as.factor(datasimple$sequence)
 datasimple$sequence <- factor(datasimple$sequence, levels = c("1", "2"), labels = c("NL-KV", "KV-NL"))
 datasimple$rank.r <- as.factor(datasimple$rank.r)
+datasimple$period <- as.factor(datasimple$period)
 
-datasimple <- subset(datasimple, select = c("id", "sequence", "notation.r", "duration", "accuracy", "sus", "rank.r"))
+# the actual dataframe we'll be working with
+df <- subset(datasimple, select = c("subject", "sequence", "period", "notation.r", "duration", "accuracy", "sus", "rank.r"))
 
 # Summary
 
