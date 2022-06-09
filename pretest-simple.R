7dc940c1979cb74026e2eed2e805c871703de2cb
Michael Feurstein <michael.feurstein@wu.ac.at>
2022-06-09 13:58:24 +0200

    check carry-over effect

--- pretest-simple.R
+++ pretest-simple.R
@@ -29,3 +29,74 @@ df %>%
   group_by(notation.r) %>%
   summarize(mean_duration = mean(duration), sd_duration = sd(duration))
 
+########
+######## CHECK CARRY-OVER EFFECT
+########
+######## Based on: https://www.lexjansen.com/pharmasug/2006/Posters/PO16.pdf
+
+# calculate and plot sum of means for sequence,period
+sp <- df %>%
+  group_by(sequence, period) %>%
+  summarize(mean_duration = mean(duration))
+
+pd = position_dodge(0)
+sp.plot <- ggplot(sp, aes(x = period,
+               y = mean_duration,
+               color = sequence,
+               group = sequence)) +
+  geom_point(shape  = 15,
+             size   = 4,
+             position = pd) +
+  geom_line() + theme(
+    legend.position = c(.99, .99),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.margin = margin(4, 4, 4, 4),
+    axis.title   = element_text(face = "bold"),
+    axis.text    = element_text(face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) +
+  ylab("Duration mean") +
+  xlab("Period") +
+  labs(color = "Sequence")
+
+# calculate and plot sum of means for notation,period
+np <- df %>%
+  group_by(notation.r, period) %>%
+  summarize(mean_duration = mean(duration))
+
+pd = position_dodge(0)
+np.plot <- ggplot(np, aes(x = period,
+               y = mean_duration,
+               color = notation.r,
+               group = notation.r)) +
+  geom_point(shape  = 15,
+             size   = 4,
+             position = pd) +
+  geom_line() + theme(
+    legend.position = c(.99, .99),
+    legend.justification = c("right", "top"),
+    legend.box.just = "right",
+    legend.margin = margin(4, 4, 4, 4),
+    axis.title   = element_text(face = "bold"),
+    axis.text    = element_text(face = "bold"),
+    plot.caption = element_text(hjust = 0)
+  ) +
+  ylab("Duration mean") +
+  xlab("Period") +
+  labs(color = "Notation")
+
+# arrange both plots in one figure
+ggarrange(sp.plot, np.plot, nrow = 1, ncol = 2)
+
+# estimate carry-over effect using sm values by t-test
+# Null Hypothesis: there is a significant difference between NL-KV / KV-NL sequences --> this means there is a carry over effect (use only period 1)
+# Alternative Hypothesis: there is no signicant difference between NL-KV / KV-NL sequences --> this means there is NO carry over effect (use both periods)
+# p-value > 0.05 shows possible carry-over effect is no significantly different between NL-KV / KV-NL sequences
+t.test(mean_duration ~ sequence, data = sp)
+
+########
+########
+########
+
+t.test(duration ~ notation.r, data = df)
