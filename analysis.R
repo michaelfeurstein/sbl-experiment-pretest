50c0f302bb71aebfbd875f050b229a3d5680cd02
Michael Feurstein <michael.feurstein@wu.ac.at>
2022-06-14 12:04:30 +0200

    current state

--- analysis.R
+++ analysis.R
@@ -1,39 +1,39 @@
-# simple design
-# crossover trial AB|BA
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
 
-datasimple <- read.csv("dataset.csv", sep = ";", dec = ",", header = TRUE)
-datasimple$time <- as.POSIXct(datasimple$time, format="%M:%S")
-datasimple$duration <- as.numeric(format(datasimple$time, "%M")) + as.numeric(format(datasimple$time, "%S"))/60
+######## IMPORT DATA  ####
+
+# crossover trial AB|BA
+# csv: https://github.com/michaelfeurstein/sbl-experiment-pretest/blob/master/dataset.csv 
+
+mydata <- read.csv("dataset.csv", sep = ";", dec = ",", header = TRUE)
+mydata$time <- as.POSIXct(mydata$time, format="%M:%S")
+mydata$duration <- as.numeric(format(mydata$time, "%M")) + as.numeric(format(mydata$time, "%S"))/60
 
 # preparations for testing
 # setup mapping and as.factor
 syntaxMapping <- c("nl" = 1, "kv" = 2)
 rankingMapping <- c("don't know" = 0, "2nd place" = 1, "1st place" = 2)
 
-datasimple$notation.r <- syntaxMapping[datasimple$notation]
-datasimple$rank.r <- rankingMapping[datasimple$rank]
+mydata$notation.r <- syntaxMapping[mydata$notation]
+mydata$rank.r <- rankingMapping[mydata$rank]
 
-datasimple$notation.r <- as.factor(datasimple$notation.r)
-datasimple$notation.r <- factor(datasimple$notation.r, levels = c("1", "2"), labels = c("natural language", "key-value"))
-datasimple$sequence <- as.factor(datasimple$sequence)
-datasimple$sequence <- factor(datasimple$sequence, levels = c("1", "2"), labels = c("NL-KV", "KV-NL"))
-datasimple$rank.r <- as.factor(datasimple$rank.r)
-datasimple$period <- as.factor(datasimple$period)
+mydata$notation.r <- as.factor(mydata$notation.r)
+mydata$notation.r <- factor(mydata$notation.r, levels = c("1", "2"), labels = c("natural language", "key-value"))
+mydata$sequence <- as.factor(mydata$sequence)
+mydata$sequence <- factor(mydata$sequence, levels = c("1", "2"), labels = c("NL-KV", "KV-NL"))
+mydata$rank.r <- as.numeric(mydata$rank.r)
+mydata$period <- as.factor(mydata$period)
+
+# log transform duration
+mydata$duration.log = log(mydata$duration)
 
 # the actual dataframe we'll be working with
-df <- subset(datasimple, select = c("subject", "sequence", "period", "notation.r", "duration", "accuracy", "sus", "rank.r"))
+df <- subset(mydata, select = c("subject", "sequence", "period", "notation.r", "duration", "duration.log", "accuracy", "sus", "rank.r"))
 
-# Summary Data
-
-# duration for sample_size.R
-df %>%
-  group_by(notation.r) %>%
-  summarize(mean_duration = mean(duration), sd_duration = sd(duration))
-
-########
-######## CHECK CARRY-OVER EFFECT
-########
-######## Based on: https://www.lexjansen.com/pharmasug/2006/Posters/PO16.pdf
+######## CHECK CARRY-OVER EFFECT ####
+# Based on: https://www.lexjansen.com/pharmasug/2006/Posters/PO16.pdf
 
 # calculate and plot sum of means for sequence,period
 sp <- df %>%
@@ -96,10 +96,63 @@ ggarrange(sp.plot, np.plot, nrow = 1, ncol = 2)
 # p-value > 0.05 shows possible carry-over effect is no significantly different between NL-KV / KV-NL sequences
 t.test(mean_duration ~ sequence, data = sp)
 
-########
-########
-########
+# based on the result of the test update the dataframe
+# option 1: leave it as it is because there is no carry over effect
+# option 2: use only period 1 of dataframe, ergo remove period 2
+
+######## SUMMARY OF DATA  ####
+
+## duration: mean, sd ####
+
+df %>%
+  group_by(notation.r) %>%
+  summarize(mean_duration = mean(duration.log), sd_duration = sd(duration.log))
+
+hist(df$duration.log[df$notation.r == "natural language"], main = "Natural Language", xlab = "Duration")
+hist(df$duration.log[df$notation.r == "key-value"], main = "Key-Value", xlab = "Duration")
+
+## accuracy: mean, sd ####
+
+df %>%
+  group_by(notation.r) %>%
+  summarize(mean_accuracy = mean(accuracy), sd_duration = sd(accuracy))
+
+hist(df$accuracy[df$notation.r == "natural language"], main = "Natural Language", xlab = "Accuracy")
+hist(df$accuracy[df$notation.r == "key-value"], main = "Key-Value", xlab = "Accuracy")
+
+## sus scores: mean, sd ####
+
+df %>%
+  group_by(notation.r) %>%
+  summarize(mean_sus_score = mean(sus), sd_sus_score = sd(sus))
+
+hist(df$sus[df$notation.r == "natural language"], main = "Natural Language", xlab = "SUS Score")
+hist(df$sus[df$notation.r == "key-value"], main = "Key-Value", xlab = "SUS Score")
+
+## ranks ####
+
+df %>%
+  group_by(notation.r) %>%
+  summarize(mean_rank = mean(rank.r), sd_sus_score = sd(rank.r))
+
+##
+## Visualization ####
+##
+
+# duration
+bxp.duration <- ggboxplot(df, x = "notation.r", xlab = "Notation", y = "duration", ylab = "log transformed duration", color = "period", palette = "jco", facet.by = "sequence", panel.labs = list(sequence = c("AB: NL->KV", "BA: KV->NL")), add = "jitter") + scale_x_discrete(labels=rep(c("NL","KV"),2))
+bxp.duration
+
+# accuracy
+bxp.accuracy <- ggboxplot(df, x = "notation.r", xlab = "Notation", y = "accuracy", ylab = "Accuracy in percent", color = "period", palette = "jco", facet.by = "sequence", panel.labs = list(sequence = c("AB: NL->KV", "BA: KV->NL")), add = "jitter") + scale_x_discrete(labels=rep(c("NL","KV"),2))
+bxp.accuracy
+
+# sus
+bxp.sus <- ggboxplot(data, x = "notation.r", xlab = "Notation", y = "sus", ylab = "SUS score", color = "experience.r", palette = "jco", facet.by = "sequence", panel.labs = list(sequence = c("AB: NL->KV", "BA: KV->NL")), add = "jitter") + scale_x_discrete(labels=rep(c("NL","KV"),2))
+bxp.sus
+
+######## ANALYSIS  ####
 
 t.test(duration ~ notation.r, data = df)
 
-
+friedman.test(rank.r ~ notation.r|subject, data = df)
--- sample_size.R
+++ sample_size.R
@@ -1,3 +1,5 @@
+library(Superpower)
+
 #
 # Power Analysis for sample size planning
 #
@@ -10,19 +12,123 @@
 # and book: https://aaroncaldwell.us/SuperpowerBook/introduction-to-power-analysis.html#overview-of-power-analysis 
 # and: https://rdrr.io/cran/Superpower/f/vignettes/more_anova_designs.Rmd 
 #
-library(Superpower)
 
-design_simple.duration <- ANOVA_design(design = "2w",
+### DURATION ####
+#### pretest data ####
+design.duration.pretest <- ANOVA_design(design = "2w",
                                        n = 6, 
-                                       mu <- c(10.0, 8.77), 
-                                       sd <- 2.765, 
+                                       mu <- c(2.27, 2.13), 
+                                       sd <- c(0.301, 0.312), 
                                        r = 0, 
                                        label_list = list("Notation" = c( "natural-language", "key-value")),
                                        plot = TRUE)
 
-power_result.simples <- ANOVA_power(design_simple.duration, 
-                                alpha = 0.05, 
-                                nsims = 100, 
-                                seed = 1234)
+plot_power(design.duration.pretest, max_n = 100, desired_power = 80)
 
-plot_power(design_simple.duration, max_n = 185, desired_power = 80)
+#### Johanson & Hasselbring (2017) data ####
+design.duration.johanson <- ANOVA_design(design = "2w",
+                                       n = 40, 
+                                       mu <- c(6.4, 2.8), 
+                                       sd <- c(5.0, 2.8), 
+                                       r = 0, 
+                                       label_list = list("Notation" = c( "gpl", "dsl")),
+                                       plot = TRUE)
+
+plot_power(design.duration.johanson, max_n = 100, desired_power = 80)
+
+#### Hoisl et al. (2014) data ####
+design.duration.hoisl <- ANOVA_design(design = "2w",
+                                         n = 20, 
+                                         mu <- c(8.67, 15.43), 
+                                         sd <- c(2.25, 6.73), 
+                                         r = 0, 
+                                         label_list = list("Notation" = c( "n", "e")),
+                                         plot = TRUE)
+
+plot_power(design.duration.hoisl, max_n = 100, desired_power = 80)
+
+
+### ACCURACY ####
+#### pretest data ####
+design.accuracy.pretest <- ANOVA_design(design = "2w",
+                                       n = 6, 
+                                       mu <- c(91.7, 80.8),
+                                       sd <- c(8.16, 15.3),
+                                       r = 0, 
+                                       label_list = list("Notation" = c( "natural-language", "key-value")),
+                                       plot = TRUE)
+
+plot_power(design.accuracy.pretest, max_n = 100, desired_power = 80)
+
+#### Johanson & Hasselbring (2017) data ####
+design.accuracy.johanson <- ANOVA_design(design = "2w",
+                                         n = 40, 
+                                         mu <- c(60.8, 97.8), 
+                                         sd <- c(47.3, 6.9), 
+                                         r = 0, 
+                                         label_list = list("Notation" = c( "gpl", "dsl")),
+                                         plot = TRUE)
+
+plot_power(design.accuracy.johanson, max_n = 100, desired_power = 80)
+
+#### Diss: Juhnke (2017) data ####
+design.accuracy.juhnke <- ANOVA_design(design = "2w",
+                                         n = 10, 
+                                         mu <- c(4.9, 1.8), 
+                                         sd <- c(2.685, 1.751), 
+                                         r = 0, 
+                                         label_list = list("Notation" = c( "nl", "dsl")),
+                                         plot = TRUE)
+
+plot_power(design.accuracy.juhnke, max_n = 100, desired_power = 80)
+
+### SUS scores ####
+#### pretest data ####
+design.sus.pretest <- ANOVA_design(design = "2w",
+                                        n = 1324, 
+                                        mu <- c(49.34, 58.21),
+                                        sd <- c(18.87, 19.59),
+                                        r = 0, 
+                                        label_list = list("Notation" = c( "natural-language", "key-value")),
+                                        plot = TRUE)
+
+plot_power(design.sus.pretest, max_n = 100, desired_power = 80)
+
+#### https://measuringu.com/sample-sizes-for-sus-comparisons/ #### --> 46
+
+#### Funk et al. (2007) data ####
+## in the study no values for sd are given thus I am using the typical sd form Jim Lewis and Jeff Sauro
+## link: https://measuringu.com/sample-sizes-for-sus-comparisons/
+design.sus.yarmand <- ANOVA_design(design = "2w",
+                                         n = 1324, 
+                                         mu <- c(78, 47), 
+                                         sd <- c(17.7, 17.7), 
+                                         r = 0, 
+                                         label_list = list("Notation" = c( "CLOnE", "protege")),
+                                         plot = TRUE)
+
+plot_power(design.sus.yarmand, max_n = 100, desired_power = 80)
+
+### Rank ####
+#### pretest data ####
+design.rank.pretest <- ANOVA_design(design = "2w",
+                                   n = 6, 
+                                   mu <- c(1.67, 1.17),
+                                   sd <- c(0.516, 0.408),
+                                   r = 0, 
+                                   label_list = list("Notation" = c( "natural-language", "key-value")),
+                                   plot = TRUE)
+
+plot_power(design.rank.pretest, max_n = 100, desired_power = 80)
+
+#### pretest data ####
+design.rank.pretest <- ANOVA_design(design = "2w",
+                                    n = 48, 
+                                    mu <- c(2.08, 1.88),
+                                    sd <- c(0.85, 0.84),
+                                    r = 0, 
+                                    label_list = list("Notation" = c( "natural-language", "key-value")),
+                                    plot = TRUE)
+
+plot_power(design.rank.pretest, max_n = 100, desired_power = 80)
+
